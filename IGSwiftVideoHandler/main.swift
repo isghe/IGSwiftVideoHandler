fea0da08943ce95e6d66d0e0d58325bc618c22fc
Isidoro Ghezzi <isidoro.ghezzi@icloud.com>
2018-04-25 17:22:37 +0200

    Automatic convert to swift 3.0 with Xcode 8.1

--- IGSwiftVideoHandler/main.swift
+++ IGSwiftVideoHandler/main.swift
@@ -22,7 +22,7 @@ class IGHMS{
 	
 	func toTime () -> CMTime{
 		let aValue: Int64 = (self.hour*3600 + self.minute * 60)*1000 + Int64 (self.second*1000)
-		let ret: CMTime = CMTime (value: aValue, timescale: 1000, flags: CMTimeFlags.Valid, epoch: CMTimeEpoch.allZeros)
+		let ret: CMTime = CMTime (value: aValue, timescale: 1000, flags: CMTimeFlags.valid, epoch: CMTimeEpoch.allZeros)
 		return ret
 	}
 	func toString () -> String{
@@ -48,15 +48,15 @@ class IGDDMMYYYY{
 }
 
 class IGVideoHandler{
-	private func getVisualFormatContraints () -> [AnyObject]{
+	fileprivate func getVisualFormatContraints () -> [AnyObject]{
 		return ["H", "V"].map {
 			return $0 + ":|-margin-[view]-margin-|"
 		}
 	}
 	
-	class private func toTime (hour: Int64, minute: Int64, second: Int64) -> CMTime{
+	class fileprivate func toTime (_ hour: Int64, minute: Int64, second: Int64) -> CMTime{
 		let aValue: Int64 = hour*3600 + minute * 60 + second
-		let ret: CMTime = CMTime (value: aValue, timescale: 1, flags: CMTimeFlags.Valid, epoch: CMTimeEpoch.allZeros)
+		let ret: CMTime = CMTime (value: aValue, timescale: 1, flags: CMTimeFlags.valid, epoch: CMTimeEpoch.allZeros)
 		return ret
 	}
 	
@@ -64,14 +64,14 @@ class IGVideoHandler{
 		fatalError(#function + "Must be overridden");
 	}
 	
-	internal func waitSemaphore (semaphore:dispatch_semaphore_t, exportSession:AVAssetExportSession){
+	internal func waitSemaphore (_ semaphore:DispatchSemaphore, exportSession:AVAssetExportSession){
 		var aCycleCounter: Int64 = 0
 		var aLastProgress: Float = 0
 		while true {
 			aCycleCounter += 1
-			let aDispatchTime: dispatch_time_t = dispatch_time(DISPATCH_TIME_NOW, 1000*1000*1000*10)
-			let aWaitResult = dispatch_semaphore_wait(semaphore, aDispatchTime)
-			let aNow: NSDate = NSDate ()
+			let aDispatchTime: DispatchTime = DispatchTime.now() + Double(1000*1000*1000*10) / Double(NSEC_PER_SEC)
+			let aWaitResult = semaphore.wait(timeout: aDispatchTime)
+			let aNow: Date = Date ()
 			let aCurrentProgress: Float = exportSession.progress
 			let aDeltaProgress: Float = aCurrentProgress - aLastProgress;
 			print("\(aNow) - aCycleCounter: \(aCycleCounter); aWaitResult: \(aWaitResult); progress: \(aCurrentProgress); delta: \(aDeltaProgress)")
@@ -92,31 +92,31 @@ class IGVideoMerger: IGVideoHandler {
 	override func doIt(){
 		let aPathStringContainer: [(path: String, ext: String)] = self.fData.sources
 		let mixComposition: AVMutableComposition = AVMutableComposition()
-		let mutableCompVideoTrack:AVMutableCompositionTrack = mixComposition.addMutableTrackWithMediaType(AVMediaTypeVideo, preferredTrackID: Int32 (kCMPersistentTrackID_Invalid))
-		let mutableCompAudioTrack:AVMutableCompositionTrack = mixComposition.addMutableTrackWithMediaType(AVMediaTypeAudio, preferredTrackID: Int32 (kCMPersistentTrackID_Invalid))
+		let mutableCompVideoTrack:AVMutableCompositionTrack = mixComposition.addMutableTrack(withMediaType: AVMediaTypeVideo, preferredTrackID: Int32 (kCMPersistentTrackID_Invalid))
+		let mutableCompAudioTrack:AVMutableCompositionTrack = mixComposition.addMutableTrack(withMediaType: AVMediaTypeAudio, preferredTrackID: Int32 (kCMPersistentTrackID_Invalid))
 		
 		var currentCMTime:CMTime = kCMTimeZero
 		var i: Int = 0
 		for aPath in aPathStringContainer{
 			i += 1
 			print ("aPath: \(aPath.path)");
-			let videoFileData: NSData = NSData (contentsOfFile: aPath.path)!
-			let randomVideoFileURL: NSURL = NSURL.fileURLWithPath ("\(self.fData.destination)\(i).\(aPath.ext)")
-			videoFileData.writeToURL (randomVideoFileURL, atomically: true)
-			let videoAsset:AVAsset = AVAsset(URL: randomVideoFileURL)
+			let videoFileData: Data = try! Data (contentsOf: URL(fileURLWithPath: aPath.path))
+			let randomVideoFileURL: URL = URL (fileURLWithPath: "\(self.fData.destination)\(i).\(aPath.ext)")
+			try? videoFileData.write (to: randomVideoFileURL, options: [.atomic])
+			let videoAsset:AVAsset = AVAsset(url: randomVideoFileURL)
 			let tracksKey: [String] = ["tracks"]
 			var aDone: Bool = false
 			
-			let semaphore:dispatch_semaphore_t = dispatch_semaphore_create(0)
-			videoAsset.loadValuesAsynchronouslyForKeys(tracksKey, completionHandler: { () -> Void in
+			let semaphore:DispatchSemaphore = DispatchSemaphore(value: 0)
+			videoAsset.loadValuesAsynchronously(forKeys: tracksKey, completionHandler: { () -> Void in
 				var error: NSError?
-				let status: AVKeyValueStatus  = videoAsset.statusOfValueForKey(tracksKey [0], error: &error)
+				let status: AVKeyValueStatus  = videoAsset.statusOfValue(forKey: tracksKey [0], error: &error)
 				
-				if status == AVKeyValueStatus.Loaded {
+				if status == AVKeyValueStatus.loaded {
 					aDone = true
 					// At this point you know the asset is ready
 					if true == aDone{
-						let aSignalResult = dispatch_semaphore_signal(semaphore)
+						let aSignalResult = semaphore.signal()
 						print("aSignalResult: \(aSignalResult)")
 					}
 				}
@@ -126,42 +126,42 @@ class IGVideoMerger: IGVideoHandler {
 			var aCycleCounter: Int64 = 0
 			while true {
 				var error: NSError?
-				let status: AVKeyValueStatus  = videoAsset.statusOfValueForKey(tracksKey [0], error: &error)
+				let status: AVKeyValueStatus  = videoAsset.statusOfValue(forKey: tracksKey [0], error: &error)
 				aCycleCounter += 1
-				let aDispatchTime: dispatch_time_t = dispatch_time(DISPATCH_TIME_NOW, 1000*1000*1000*10)
-				let aWaitResult = dispatch_semaphore_wait(semaphore, aDispatchTime)
-				let aNow: NSDate = NSDate ()
+				let aDispatchTime: DispatchTime = DispatchTime.now() + Double(1000*1000*1000*10) / Double(NSEC_PER_SEC)
+				let aWaitResult = semaphore.wait(timeout: aDispatchTime)
+				let aNow: Date = Date ()
 				print("\(aNow) - aCycleCounter: \(aCycleCounter); aWaitResult: \(aWaitResult); status: \(status.rawValue)")
 				if 0 == aWaitResult{
 					break;
 				}
 			}
 			
-			var aVideo: [AnyObject] = videoAsset.tracksWithMediaType (AVMediaTypeVideo)
-			var aAudio: [AnyObject] = videoAsset.tracksWithMediaType (AVMediaTypeAudio)
+			var aVideo: [AnyObject] = videoAsset.tracks (withMediaType: AVMediaTypeVideo)
+			var aAudio: [AnyObject] = videoAsset.tracks (withMediaType: AVMediaTypeAudio)
 			if "mpeg" == aPath.ext {
 				let instruction:AVMutableVideoCompositionInstruction = AVMutableVideoCompositionInstruction ()
 				instruction.timeRange = CMTimeRangeMake(kCMTimeZero, videoAsset.duration)
 				// instruction.set
 				let transformer:AVMutableVideoCompositionLayerInstruction = AVMutableVideoCompositionLayerInstruction (assetTrack: aVideo [0] as! AVAssetTrack)
 				let finalTransform:CGAffineTransform = CGAffineTransform ()
-				transformer.setTransform(finalTransform, atTime:kCMTimeZero)
-				transformer.setCropRectangle(CGRect(origin: CGPoint (x: 0, y: 0), size: CGSize (width: 960, height: 540)), atTime: kCMTimeZero)
+				transformer.setTransform(finalTransform, at:kCMTimeZero)
+				transformer.setCropRectangle(CGRect(origin: CGPoint (x: 0, y: 0), size: CGSize (width: 960, height: 540)), at: kCMTimeZero)
 			}
 			else{
 				do {
-					try mutableCompVideoTrack.insertTimeRange (CMTimeRangeMake(kCMTimeZero, videoAsset.duration), ofTrack: aVideo [0] as! AVAssetTrack, atTime:currentCMTime)
+					try mutableCompVideoTrack.insertTimeRange (CMTimeRangeMake(kCMTimeZero, videoAsset.duration), of: aVideo [0] as! AVAssetTrack, at:currentCMTime)
 				} catch _ {
 				}
 				do {
-					try mutableCompAudioTrack.insertTimeRange (CMTimeRangeMake(kCMTimeZero, videoAsset.duration), ofTrack: aAudio[0] as! AVAssetTrack, atTime:currentCMTime)
+					try mutableCompAudioTrack.insertTimeRange (CMTimeRangeMake(kCMTimeZero, videoAsset.duration), of: aAudio[0] as! AVAssetTrack, at:currentCMTime)
 				} catch _ {
 				}
 			}
 			currentCMTime = CMTimeAdd(currentCMTime, videoAsset.duration);
 		}
 		
-		let randomFinalVideoFileURL: NSURL = NSURL.fileURLWithPath ("\(self.fData.destination).mp4")
+		let randomFinalVideoFileURL: URL = URL (fileURLWithPath: "\(self.fData.destination).mp4")
 		let exportSession:AVAssetExportSession = AVAssetExportSession (asset: mixComposition, presetName: AVAssetExportPreset960x540)!
 		exportSession.outputFileType = AVFileTypeMPEG4
 		exportSession.outputURL = randomFinalVideoFileURL
@@ -173,33 +173,33 @@ class IGVideoMerger: IGVideoHandler {
 		
 		var aCounter: Int64 = 0
 		var aDone: Bool = false
-		let semaphore:dispatch_semaphore_t = dispatch_semaphore_create(0)
-		exportSession.exportAsynchronouslyWithCompletionHandler({
+		let semaphore:DispatchSemaphore = DispatchSemaphore(value: 0)
+		exportSession.exportAsynchronously(completionHandler: {
 			aCounter += 1
 			let aStatus: AVAssetExportSessionStatus = exportSession.status
 			print ("aCounter: \(aCounter), status: \(aStatus.rawValue)")
 			
 			switch (aStatus){
-			case AVAssetExportSessionStatus.Completed:
+			case AVAssetExportSessionStatus.completed:
 				aDone = true
 				break
-			case AVAssetExportSessionStatus.Waiting:
+			case AVAssetExportSessionStatus.waiting:
 				break
-			case AVAssetExportSessionStatus.Exporting:
+			case AVAssetExportSessionStatus.exporting:
 				break
-			case AVAssetExportSessionStatus.Failed:
+			case AVAssetExportSessionStatus.failed:
 				print("exportSession.error: \(exportSession.error)")
 				aDone = true
 				break
-			case AVAssetExportSessionStatus.Cancelled:
+			case AVAssetExportSessionStatus.cancelled:
 				print("exportSession.error: \(exportSession.error)")
 				aDone = true
 				break
-			case AVAssetExportSessionStatus.Unknown:
+			case AVAssetExportSessionStatus.unknown:
 				break
 			}
 			if true == aDone{
-				let aSignalResult = dispatch_semaphore_signal(semaphore)
+				let aSignalResult = semaphore.signal()
 				print("aSignalResult: \(aSignalResult)")
 			}
 			
@@ -216,27 +216,27 @@ class IGVideoExtractor: IGVideoHandler {
 	override func doIt(){
 		print(AVURLAsset.audiovisualTypes())
 		
-		let aURL: NSURL = NSURL.fileURLWithPath (self.fData.source)
-		let aAsset: AVAsset = AVURLAsset (URL: aURL, options: nil)
+		let aURL: URL = URL (fileURLWithPath: self.fData.source)
+		let aAsset: AVAsset = AVURLAsset (url: aURL, options: nil)
 		print(aAsset)
 		print(aAsset.trackGroups)
 		print(aAsset.tracks)
 		
-		let movieTracks: [AnyObject] = aAsset.tracksWithMediaType (AVMediaTypeVideo);
+		let movieTracks: [AnyObject] = aAsset.tracks (withMediaType: AVMediaTypeVideo);
 		print(movieTracks)
 		assert (1 == movieTracks.count);
 		
-		let compatiblePresets: [AnyObject] = AVAssetExportSession.exportPresetsCompatibleWithAsset (aAsset)
+		let compatiblePresets: [AnyObject] = AVAssetExportSession.exportPresets (compatibleWith: aAsset) as [AnyObject]
 		print("compatiblePresets: \(compatiblePresets)")
 		if (compatiblePresets as! [String]).contains(AVAssetExportPreset960x540){
-			let queue:dispatch_queue_t = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
-			let group:dispatch_group_t  = dispatch_group_create();
+			let queue:DispatchQueue = DispatchQueue.global(priority: DispatchQueue.GlobalQueuePriority.default);
+			let group:DispatchGroup  = DispatchGroup();
 			for aCurrentTimeRange in self.fData.ranges{
-				dispatch_group_async(group, queue,{
+				queue.async(group: group,execute: {
 					print ("start: \(aCurrentTimeRange.start.toString())")
 					print ("end: \(aCurrentTimeRange.end.toString())")
 					let exportSession: AVAssetExportSession = AVAssetExportSession (asset: aAsset, presetName: AVAssetExportPreset960x540)!
-					exportSession.outputURL = NSURL.fileURLWithPath (String(format:"%@_%@_%@_%@.mp4", self.fData.title, self.fData.date.toString(), aCurrentTimeRange.start.toString(), aCurrentTimeRange.end.toString()))
+					exportSession.outputURL = URL (fileURLWithPath: String(format:"%@_%@_%@_%@.mp4", self.fData.title, self.fData.date.toString(), aCurrentTimeRange.start.toString(), aCurrentTimeRange.end.toString()))
 					print ("exportSession.outputURL: \(exportSession.outputURL)");
 					exportSession.outputFileType = AVFileTypeMPEG4
 					
@@ -246,40 +246,40 @@ class IGVideoExtractor: IGVideoHandler {
 					print ("exportSession.estimatedOutputFileLength: \(exportSession.estimatedOutputFileLength)")
 					var aCounter: Int64 = 0
 					var aDone: Bool = false
-					let semaphore:dispatch_semaphore_t = dispatch_semaphore_create(0)
-					exportSession.exportAsynchronouslyWithCompletionHandler({
+					let semaphore:DispatchSemaphore = DispatchSemaphore(value: 0)
+					exportSession.exportAsynchronously(completionHandler: {
 						aCounter += 1
 						let aStatus: AVAssetExportSessionStatus = exportSession.status
 						print ("aCounter: \(aCounter), status: \(aStatus.rawValue)")
 						
 						switch (aStatus){
-						case AVAssetExportSessionStatus.Completed:
+						case AVAssetExportSessionStatus.completed:
 							aDone = true
 							break
-						case AVAssetExportSessionStatus.Waiting:
+						case AVAssetExportSessionStatus.waiting:
 							break
-						case AVAssetExportSessionStatus.Exporting:
+						case AVAssetExportSessionStatus.exporting:
 							break
-						case AVAssetExportSessionStatus.Failed:
+						case AVAssetExportSessionStatus.failed:
 							print("exportSession.error: \(exportSession.error)")
 							aDone = true
 							break
-						case AVAssetExportSessionStatus.Cancelled:
+						case AVAssetExportSessionStatus.cancelled:
 							print("exportSession.error: \(exportSession.error)")
 							aDone = true
 							break
-						case AVAssetExportSessionStatus.Unknown:
+						case AVAssetExportSessionStatus.unknown:
 							break
 						}
 						if true == aDone{
-							let aSignalResult = dispatch_semaphore_signal(semaphore)
+							let aSignalResult = semaphore.signal()
 							print("aSignalResult: \(aSignalResult)")
 						}
 					})
 					self.waitSemaphore(semaphore, exportSession: exportSession)
 				})
 			}
-			dispatch_group_wait(group, DISPATCH_TIME_FOREVER)
+			group.wait(timeout: DispatchTime.distantFuture)
 			// dispatch_release(group)
 		}
 	}
@@ -292,8 +292,8 @@ class IGVideoTester: IGVideoHandler {
 			(path:"test2", ext: "mp4")
 		]
 		for aPath:(path: String, ext: String) in aPathStringContainer{
-			let aVideoFileURL: NSURL = NSURL.fileURLWithPath (aPath.path)
-			let videoAsset:AVAsset = AVAsset(URL: aVideoFileURL)
+			let aVideoFileURL: URL = URL (fileURLWithPath: aPath.path)
+			let videoAsset:AVAsset = AVAsset(url: aVideoFileURL)
 			let seconds = CMTimeGetSeconds (videoAsset.duration)
 			print("videoAsset.duration: \(seconds) s;")
 		}
